08a969394b415620ce997b9e9f8f8530c8bab300
Erin Franke <erinfranke23@gmail.com>
2024-12-26 16:00:19 -0600

    another idea

--- brainstorming.R
+++ brainstorming.R
@@ -14,4 +14,8 @@ system.time(fread('https://raw.githubusercontent.com/rfordatascience/tidytuesday
 # 1) Create a tutorial showing how to think about "the six main verbs" (select, filter, mutate, arrange, group_by, summarize) with data.table
 # and provide exercises to practice these
 # https://ggplot-dplyr-intro.netlify.app/#Wrangling_data_with_dplyr_functions could model off the data wrangling section of this
-# 2) 
+# 1.5) Similar to idea 1. But we could have a presentation that really focuses on how to teach data wrangling visually for students totally new to it
+##  - see cheatsheet in this repo which is a basic version of doing this.
+# 2) Data.table for computer scientists. Might be harder because we are not a computer scientists and think like statisticians so not sure 
+# if we could replicate how they think about data wrangling. But because data.table is more similar to "computer programming" we think that 
+# it could be easier for students with a comp sci background to learn that rather than tidyverse?
