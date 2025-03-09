e2dc3c5b4355960e11f6607baebe172d3d151981
Sara Colando <skcolando@gmail.com>
2025-03-09 12:35:35 -0400

    speed tests for pivoting and joins as well as some demo material

--- livedemo.R
+++ livedemo.R
@@ -4,6 +4,38 @@ library(dtplyr)
 library(tidyverse)
 library(tidytable)
 
+# live demo using House prices in Ames, Iowa data from the CMU S&DS Data Repository
+
+# originally from: D De Cock (2011). Ames, Iowa: Alternative to the Boston Housing
+# Data as an End of Semester Regression Project. Journal of Statistics Education 19 (3).
+# https://doi.org/10.1080/10691898.2011.11889627
+
+# loading in the data
+system.time(house_prices <- fread("demo_data/ames-housing.csv")) # data.table
+system.time(house_prices2 <- read_csv("demo_data/ames-housing.csv")) # readr
+system.time(house_prices3 <- read.csv("demo_data/ames-housing.csv")) # base r
+
+house_prices <- data.table(house_prices)
+
+# %like% and %between% logical operators
+
+# let's say we want to subset to only houses with warranty deeds
+house_prices[Sale.Type %like% 'WD',]
+# note that %like% is case sensitive 
+house_prices[Sale.Type %like% 'wd',]
+# if we want to ingore case, we would need to use tolower()
+house_prices[tolower(Sale.Type) %like% 'wd', ]
+
+
+# now, let's say we want to just look at houses with prices in the IQR 
+quantile(house_prices$SalePrice, c(0.25, .75))
+IQR_houses = house_prices[SalePrice %between% c(129500, 213500),]
+
+# rolling joins
+
+
+# grouped modeling
+
 
 
 
